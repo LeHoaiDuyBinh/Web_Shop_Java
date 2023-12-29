7d6961bb8e9c231fdbce9cf3887b2b8880d57e32
Tuankiet12s <nguyentuankiet040202@gmail.com>
2023-12-29 12:33:19 +0700

    captcha cho login

--- src/main/java/com/example/web_shop_ptit/client/controller/LoginUserController.java
+++ src/main/java/com/example/web_shop_ptit/client/controller/LoginUserController.java
@@ -1,6 +1,7 @@
 package com.example.web_shop_ptit.client.controller;
 
-import com.example.web_shop_ptit.client.entity.Category;
+import cn.apiclub.captcha.Captcha;
+import com.example.web_shop_ptit.client.config.CaptchaGenerator;
 import com.example.web_shop_ptit.client.entity.Customer;
 import com.example.web_shop_ptit.client.entity.RegistrationInfo;
 import com.example.web_shop_ptit.client.service.CustomerService;
@@ -9,10 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,25 +21,44 @@ public class LoginUserController {
     private CustomerService customerService;
 
     @GetMapping("login")
-    public String loginPage(){
+    public String loginPage(Model model) {
         List<Customer> listCustomer = CustomerService.listAll();
 //        for (Customer customer : listCustomer) {
 //            System.out.println(customer.getEmail() + customer.getPassword());
 //        }
+        Customer customer = new Customer();
+        getCaptcha(customer);
+        model.addAttribute("customer", customer);
         return "web_client/login";
     }
 
+    private void getCaptcha(Customer customer) {
+        Captcha capcha = CaptchaGenerator.createCaptcha(120, 80);
+        customer.setHiddenCaptcha(capcha.getAnswer());
+        customer.setCaptcha("");
+        customer.setRealCaptcha(CaptchaGenerator.encodeCaptchatoBinary(capcha));
+    }
+
     @PostMapping("login")
-    public String login(@RequestParam String email,@RequestParam String password, Model model, HttpServletRequest request) {
+    public String login(@RequestParam String email, @RequestParam String password, Model model, HttpServletRequest request,
+                        @ModelAttribute Customer customer1) {
         boolean checkLogin = customerService.checkLogin(email, password);
-        if(checkLogin) {
+        if (checkLogin && customer1.getCaptcha().equals(customer1.getHiddenCaptcha())) {
             HttpSession session = request.getSession();
             Customer customer = customerService.findByEmail(email);
             session.setAttribute("customerInfor", new RegistrationInfo(customer.getFullName(), customer.getEmail(), customer.getPassword(), customer.getPhoneNumber()));
             return "redirect:/";
-        }else{
+        } else if(!customer1.getCaptcha().equals(customer1.getHiddenCaptcha())){
+            model.addAttribute("message", "Invalid Captcha");
+            getCaptcha(customer1);
+            model.addAttribute("customer", customer1);
+            return "web_client/login";
+        }else {
+            getCaptcha(customer1);
+            model.addAttribute("customer", customer1);
             model.addAttribute("error", "Invalid email or password");
             return "web_client/login";
         }
     }
+
 }
--- src/main/java/com/example/web_shop_ptit/client/entity/Customer.java
+++ src/main/java/com/example/web_shop_ptit/client/entity/Customer.java
@@ -27,6 +27,37 @@ public class Customer {
     @JoinColumn(name = "email", referencedColumnName = "email")
     private ShoppingCart shoppingCart;
 
+    @Transient
+    private String captcha;
+    @Transient
+    private String hiddenCaptcha;
+    @Transient
+    private String realCaptcha;
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
+
+    public String getHiddenCaptcha() {
+        return hiddenCaptcha;
+    }
+
+    public void setHiddenCaptcha(String hiddenCaptcha) {
+        this.hiddenCaptcha = hiddenCaptcha;
+    }
+
+    public String getRealCaptcha() {
+        return realCaptcha;
+    }
+
+    public void setRealCaptcha(String realCaptcha) {
+        this.realCaptcha = realCaptcha;
+    }
+
     public Customer() {
     }
 
