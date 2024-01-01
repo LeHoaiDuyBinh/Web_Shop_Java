6ff25a891d38b6f28ea09e2d17998f3719ee9463
Đạt Tấn <nguyentandat01102002@gmail.com>
2024-01-01 23:57:00 +0700

    phan quyen customer

--- src/main/java/com/example/web_shop_ptit/admin/controller/CustomerAdminController.java
+++ src/main/java/com/example/web_shop_ptit/admin/controller/CustomerAdminController.java
@@ -1,7 +1,10 @@
 package com.example.web_shop_ptit.admin.controller;
 
+import com.example.web_shop_ptit.admin.entity.Admin;
 import com.example.web_shop_ptit.admin.entity.CustomerManagement;
 import com.example.web_shop_ptit.admin.service.CustomerManagementService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/admin/")
@@ -19,17 +23,26 @@ public class CustomerAdminController {
     @Autowired
     private CustomerManagementService customerManagementService;
     @GetMapping("/customer")
-    public String customerPage(Model model) {
+    public String customerPage(Model model, HttpServletRequest request) {
         List<CustomerManagement> customers = customerManagementService.listAll();
 
         model.addAttribute("customers", customers);
+        HttpSession session = request.getSession();
+        Admin admin = (Admin) session.getAttribute("adminInfo");
+        model.addAttribute("role", admin.getRole());
         // Trả về tên của view hoặc đường dẫn
         return "web_admin/customer";
     }
     @RequestMapping("/updateCustomer")
     public String updateCustomerPage(@RequestParam String Mail,
-                                     Model model) {
+                                     Model model, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         try {
+            HttpSession session = request.getSession();
+            Admin admin = (Admin) session.getAttribute("adminInfo");
+            if(!Objects.equals(admin.getRole(), "admin") && !Objects.equals(admin.getRole(), "manager")){
+                redirectAttributes.addFlashAttribute("err", "Bạn không có quyền thực hiện thao tác này!");
+                return "redirect:/admin/customer";
+            }
             CustomerManagement customer = customerManagementService.getCustomerByEmail(Mail);
             model.addAttribute("customer", customer);
             model.addAttribute("operation", "editCustomer");
